ad62f0f197630e1f4e6ac93861b764ad9ae71a2c
LZimmer <zimmer.lucas.w@gmail.com>
2025-09-28 14:20:09 +0200

    Preprocessing example added

---
+++ scripts/single_nifti_preprocessing.py
@@ -0,0 +1,26 @@
+import argparse
+from pathlib import Path
+from predict_gbm.preprocessing import NiftiPreprocessor
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-cuda_device", type=str, default="0", help="GPU id to run on.")
+    args = parser.parse_args()
+
+    patient_root_dir = Path(
+        "/mnt/Drive2/lucas/datasets/RHUH-GBM/Images/NIfTI/RHUH-GBM/RHUH-0001"
+    )
+
+    processor = NiftiPreprocessor(
+        t1_file=patient_root_dir / "RHUH-0001_0_t1.nii.gz",
+        t1c_file=patient_root_dir / "RHUH-0001_0_t1ce.nii.gz",
+        t2_file=patient_root_dir / "RHUH-0001_0_t2.nii.gz",
+        flair_file=patient_root_dir / "RHUH-0001_0_flair.nii.gz",
+        pre_treatment=True,
+        outdir=patient_root_dir,
+        cuda_device=args.cuda_device,
+        is_skull_stripped=True,
+        is_coregistered=False,
+    )
+    processor.run()
