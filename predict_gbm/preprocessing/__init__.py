c4304010606b8e8092eac8bbb181ac999ded3e49
LMZimmer <zimmer.lucas.w@gmail.com>
2026-08-06 15:40:02 +0200

    Add antsAtropos as tissue segmentation algorithm
    Make run_tissue_seg a dispatcher for atlas registration based
    segmentation and antsAtropos

--- predict_gbm/preprocessing/__init__.py
+++ predict_gbm/preprocessing/__init__.py
@@ -1,7 +1,7 @@
 from .preprocess import DicomPreprocessor, NiftiPreprocessor
 from .conversion import dicom_to_nifti
 from .tumor_segmentation import run_brats
-from .tissue_segmentation import run_tissue_seg_registration
+from .tissue_segmentation import run_tissue_seg
 from .norm_ss_coregistration import norm_ss_coregister, register_recurrence
 
 __all__ = [
@@ -11,5 +11,5 @@ __all__ = [
     "norm_ss_coregister",
     "register_recurrence",
     "run_brats",
-    "run_tissue_seg_registration",
+    "run_tissue_seg",
 ]
--- scripts/stepwise_processing.py
+++ scripts/stepwise_processing.py
@@ -8,7 +8,7 @@ from predict_gbm.preprocessing import (
     register_recurrence,
 )
 from predict_gbm.preprocessing import DicomPreprocessor
-from predict_gbm.preprocessing import run_tissue_seg_registration
+from predict_gbm.preprocessing import run_tissue_seg
 from predict_gbm.preprocessing import run_brats
 from predict_gbm.prediction import predict_tumor_growth
 from predict_gbm.evaluation import evaluate_tumor_model
@@ -110,7 +110,7 @@ if __name__ == "__main__":
     )
 
     # Tissue segmentation
-    run_tissue_seg_registration(
+    run_tissue_seg(
         t1_file=skull_strip_followup_outdir / "t1c_skullstripped.nii.gz",
         outdir=outdir_followup,
     )
